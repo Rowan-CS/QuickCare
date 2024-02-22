1c14ed7fe722499156463e232f0c83f26ae40e48
724877058@qq.com <724877058@qq.com>
2024-02-22 18:55:57 +0800

    first commit

---
+++ src/main/java/com/rw/quickcare/QuickcareApplication.java
@@ -0,0 +1,13 @@
+package com.rw.quickcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rw/quickcare/QuickcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rw.quickcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
